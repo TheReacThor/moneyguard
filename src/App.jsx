72322a4e9b4123bb6d1d99385b196e6ac7a77798
8kaze8 <kadir.zeyrek1@gmail.com>
2025-03-01 16:30:28 +0300

    feat: Add routing and initial DashboardPage component
    Implemented basic routing using react-router-dom and created a placeholder DashboardPage with basic styling. Added routes for dashboard and a temporary fallback route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+// Other imports will be added as they are implemented
+
 function App() {
   return (
-    <>
-      <h1>APP</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes will be added here */}
+        <Route path="/dashboard" element={<DashboardPage />} />
+        {/* PrivateRoute wrapper will be added when authentication is implemented */}
+        <Route path="*" element={<DashboardPage />} />{" "}
+        {/* Temporary fallback */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
