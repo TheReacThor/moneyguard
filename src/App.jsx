19d1fe8a6631946a03c1e745b95d439575044e03
Onder Yilmaz <onderxyilmaz@gmail.com>
2025-03-02 01:21:51 +0300

    Task 14: HomeTab Component'inin Oluşturulması

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import HomeTab from "./components/HomeTab/HomeTab";
 // Other imports will be added as they are implemented
 
 function App() {
@@ -7,9 +8,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* Public routes will be added here */}
+        <Route path="/home" element={<HomeTab />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         {/* PrivateRoute wrapper will be added when authentication is implemented */}
-        <Route path="*" element={<DashboardPage />} />{" "}
+        <Route path="*" element={<DashboardPage />} />
         {/* Temporary fallback */}
       </Routes>
     </BrowserRouter>
---
+++ src/components/HomeTab/HomeTab.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import TransactionList from '../TransactionList/TransactionList'; // Sonra oluşturulacak
+import Balance from '../Balance/Balance'; // Sonra oluşturulacak
+import { fetchTransactions } from '../../redux/Transactions/slice'; // Task 13'ten
+import styles from './HomeTab.module.css';
+
+const HomeTab = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTransactions());
+  }, [dispatch]);
+
+  return (
+    <div className={styles.container}>
+      <Balance />
+      <TransactionList />
+    </div>
+  );
+};
+
+export default HomeTab;
