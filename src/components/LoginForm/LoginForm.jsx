a7032b2a02124f70855dacb7c92b69bbd69ad88f
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2025-03-01 23:27:13 -0800

    Refactor Login and Registration forms for improved layout and responsiveness; update styles and media queries

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -14,55 +14,57 @@ function LoginForm() {
 
   return (
     <div className={style.background}>
-      <div className={style.title}>
-        <img src={moneyGuardIcon} alt='Money Guard Icon' />
-        <h3 className={style.titleText}>Money Guard</h3>
-      </div>
+      <div className={style.container}>
+        <div className={style.title}>
+          <img className={style.moneyGuardIcon} src={moneyGuardIcon} alt='Money Guard Icon' />
+          <h3 className={style.titleText}>Money Guard</h3>
+        </div>
 
-      <Formik
-        initialValues={initialValues}
-        validationSchema={loginValidationSchema}
-        onSubmit={(values, action) => {
-          console.log(values);
-          action.resetForm();
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className={style.form}>
-            <div className={style.inputWrapper}>
-              <div className={style.inputContainer}>
-                <img src={emailIcon} alt='Email Icon' className={style.icon} />
-                <Field className={style.input} type='email' name='email' placeholder='E-mail' />
+        <Formik
+          initialValues={initialValues}
+          validationSchema={loginValidationSchema}
+          onSubmit={(values, action) => {
+            console.log(values);
+            action.resetForm();
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className={style.form}>
+              <div className={style.inputWrapper}>
+                <div className={style.inputContainer}>
+                  <img src={emailIcon} alt='Email Icon' className={style.icon} />
+                  <Field className={style.input} type='email' name='email' placeholder='E-mail' />
+                </div>
+                <ErrorMessage name='email' component='div' className={style.error} />
               </div>
-              <ErrorMessage name='email' component='div' className={style.error} />
-            </div>
-            <div className={style.inputWrapper}>
-              <div className={style.inputContainer}>
-                <img src={passwordIcon} alt='Password Icon' className={style.icon} />
-                <Field
-                  className={style.input}
-                  type='password'
-                  name='password'
-                  placeholder='Password'
-                />
+              <div className={style.inputWrapper}>
+                <div className={style.inputContainer}>
+                  <img src={passwordIcon} alt='Password Icon' className={style.icon} />
+                  <Field
+                    className={style.input}
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                  />
+                </div>
+                <ErrorMessage name='password' component='div' className={style.error} />
               </div>
-              <ErrorMessage name='password' component='div' className={style.error} />
-            </div>
 
-            {/* Strong Pass ile ilgili Library'e bakicam... */}
-            <div className={style.buttons}>
-              <button type='submit' className={style.buttonRegister} disabled={isSubmitting}>
-                LOG IN
-              </button>
-              <Link to='/register'>
-                <button type='button' className={style.buttonLogin}>
-                  REGISTER
+              {/* Strong Pass ile ilgili Library'e bakicam... */}
+              <div className={style.buttons}>
+                <button type='submit' className={style.buttonRegister} disabled={isSubmitting}>
+                  LOG IN
                 </button>
-              </Link>
-            </div>
-          </Form>
-        )}
-      </Formik>
+                <Link to='/register'>
+                  <button type='button' className={style.buttonLogin}>
+                    REGISTER
+                  </button>
+                </Link>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -18,79 +18,80 @@ function RegisterForm() {
 
   return (
     <div className={style.background}>
-      <div className={style.title}>
-        <img src={moneyGuardIcon} alt='Money Guard Icon' />
-        <h3 className={style.titleText}>Money Guard</h3>
-      </div>
+      <div className={style.container}>
+        <div className={style.title}>
+          <img className={style.moneyGuardIcon} src={moneyGuardIcon} alt='Money Guard Icon' />
+          <h3 className={style.titleText}>Money Guard</h3>
+        </div>
 
-      <Formik
-        initialValues={initialValues}
-        validationSchema={registrationValidationSchema}
-        onSubmit={(values, actions) => {
-          console.log(values);
-          actions.resetForm();
-        }}
-      >
-        {({ isSubmitting, values }) => (
-          <Form className={style.form}>
-            <div className={style.inputWrapper}>
-              <div className={style.inputContainer}>
-                <img src={nameIcon} alt='Name Icon' className={style.icon} />
-                <Field className={style.input} type='text' name='name' placeholder='Name' />
+        <Formik
+          initialValues={initialValues}
+          validationSchema={registrationValidationSchema}
+          onSubmit={(values, actions) => {
+            console.log(values);
+            actions.resetForm();
+          }}
+        >
+          {({ isSubmitting, values }) => (
+            <Form className={style.form}>
+              <div className={style.inputWrapper}>
+                <div className={style.inputContainer}>
+                  <img src={nameIcon} alt='Name Icon' className={style.icon} />
+                  <Field className={style.input} type='text' name='name' placeholder='Name' />
+                </div>
+                <ErrorMessage name='name' component='div' className={style.error} />
               </div>
-              <ErrorMessage name='name' component='div' className={style.error} />
-            </div>
-            <div className={style.inputWrapper}>
-              <div className={style.inputContainer}>
-                <img src={emailIcon} alt='Email Icon' className={style.icon} />
-                <Field className={style.input} type='email' name='email' placeholder='E-mail' />
+              <div className={style.inputWrapper}>
+                <div className={style.inputContainer}>
+                  <img src={emailIcon} alt='Email Icon' className={style.icon} />
+                  <Field className={style.input} type='email' name='email' placeholder='E-mail' />
+                </div>
+                <ErrorMessage name='email' component='div' className={style.error} />
               </div>
-              <ErrorMessage name='email' component='div' className={style.error} />
-            </div>
-            <div className={style.inputWrapper}>
-              <div className={style.inputContainer}>
-                <img src={passwordIcon} alt='Password Icon' className={style.icon} />
-                <Field
-                  className={style.input}
-                  type='password'
-                  name='password'
-                  placeholder='Password'
+              <div className={style.inputWrapper}>
+                <div className={style.inputContainer}>
+                  <img src={passwordIcon} alt='Password Icon' className={style.icon} />
+                  <Field
+                    className={style.input}
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                  />
+                </div>
+                <ErrorMessage name='password' component='div' className={style.error} />
+              </div>
+              <div className={style.inputWrapper}>
+                <div className={style.inputContainer}>
+                  <img src={passwordIcon} alt='Confirm Password Icon' className={style.icon} />
+                  <Field
+                    className={style.input}
+                    type='password'
+                    name='confirmPassword'
+                    placeholder='Confirm Password'
+                  />
+                </div>
+                <ErrorMessage name='confirmPassword' component='div' className={style.error} />
+                <PasswordStrengthBar
+                  password={values.password}
+                  className={style.passwordStrengthBar}
+                  scoreWords={[]}
+                  //  /* still veremedim buna bakicam tekrardan */
                 />
               </div>
-              <ErrorMessage name='password' component='div' className={style.error} />
-            </div>
-            <div className={style.inputWrapper}>
-              <div className={style.inputContainer}>
-                <img src={passwordIcon} alt='Confirm Password Icon' className={style.icon} />
-                <Field
-                  className={style.input}
-                  type='password'
-                  name='confirmPassword'
-                  placeholder='Confirm Password'
-                />
-              </div>
-              <ErrorMessage name='confirmPassword' component='div' className={style.error} />
-              <PasswordStrengthBar
-                password={values.password}
-                className={style.passwordStrengthBar}
-                scoreWords={[]}
-                scoreColors={['#ffb627', '#ffb627', '#ffb627', '#ffb627']}
-                //  /* still veremedim buna bakicam tekrardan */
-              />
-            </div>
-            <div className={style.buttons}>
-              <button type='submit' className={style.buttonRegister} disabled={isSubmitting}>
-                REGISTER
-              </button>
-              <Link to='/login'>
-                <button type='button' className={style.buttonLogin}>
-                  LOG IN
+              <div className={style.buttons}>
+                <button type='submit' className={style.buttonRegister} disabled={isSubmitting}>
+                  REGISTER
                 </button>
-              </Link>
-            </div>
-          </Form>
-        )}
-      </Formik>
+                <Link to='/login'>
+                  <button type='button' className={style.buttonLogin}>
+                    LOG IN
+                  </button>
+                </Link>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
