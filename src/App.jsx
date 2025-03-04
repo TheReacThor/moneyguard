f3a27cec1701c7f321c765d15d4da6f2f00c4090
Onder Yilmaz <onderxyilmaz@gmail.com>
2025-03-04 19:23:02 +0300

    Görev 23: StatisticsTab Component'inin Oluşturulması

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import Statistics from "./components/Statistics/Statistics";
 // Other imports will be added as they are implemented
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         {/* Public routes will be added here */}
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/statistics" element={<Statistics />} />
         {/* PrivateRoute wrapper will be added when authentication is implemented */}
         <Route path="*" element={<DashboardPage />} />{" "}
         {/* Temporary fallback */}
