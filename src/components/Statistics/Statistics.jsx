d8340edc0cb37b9ed7535c0bbe05e81812477d78
Onder Yilmaz <onderxyilmaz@gmail.com>
2025-03-04 19:42:22 +0300

    Fix: Arkaplan sorunu çözüldü - Statistics.jsx dosyasındaki gereksiz importlar kaldırıldı

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Chart from '../ChartDoughnut/ChartDoughnut';
-import StatisticsDashboard from '../StatisticsSelector/StatisticsSelector';
 import StatisticsTable from './StatisticsTable';
 import styles from './Statistics.module.css';
 
 const Statistics = () => {
   return (
     <div className={styles.container}>
-      <StatisticsDashboard />
-      <Chart />
       <StatisticsTable />
     </div>
   );
